c4da2369e40b9c01ade9997ef2c1e6aff9bfc71c
Stephen Thurman <thurman024@gmail.com>
2018-01-16 19:23:53 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  turns_played = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns_played += 1
+    end
+  end
+  return turns_played
+end
+
+def current_player(board)
+  turns_played = turn_count(board)
+  #if turns_played == 0 || turns_played % 2 == 0
+  #  "X"
+  #elsif turns_played % 2 != 0
+  #  "O"
+  #end
+  return turns_played.even? ? "X" : "O"
+end
