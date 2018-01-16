9c492d51729922cb5b4f269e12e3450b8af1c7cc
Stephen Thurman <thurman024@gmail.com>
2018-01-16 19:15:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,13 +5,14 @@ def turn_count(board)
       turns_played += 1
     end
   end
+  return turns_played
 end
 
 def current_player(board)
   turns_played = turn_count(board)
-  if turn_count == 0 || turn_count % 2 == 0
+  if turns_played == 0 || turns_played % 2 == 0
     "X"
-  elsif turn_count % 2 != 0
+  elsif turns_played % 2 != 0
     "O"
   end
 end
